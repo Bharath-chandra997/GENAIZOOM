49d0ed281fdd2b16dfdc2b0fbf044c811912120f
Bharath-chandra997 <bandabharathchandra@gmail.com>
2025-10-20 10:31:05 +0530

    initial commit

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
+import './index.css'; // ✅ Make sure to include this for Tailwind or custom CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
